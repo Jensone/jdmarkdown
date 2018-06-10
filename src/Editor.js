8336c2da3649424596684ce165b35cfcbe0466be
Jensone <hello@jensone.com>
2018-06-10 16:29:34 +0200

    Ajout du module Marked

--- src/Editor.js
+++ src/Editor.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './Editor.css';
+// MD insctructions
 import { sampleText } from './sampleText';
+// Marked.js
+import marked from 'marked';
 
 class App extends Component {
+
+  state = {
+    text: sampleText
+  }
+
+  editText = (event) => {
+    const text = event.target.value;
+    this.setState({ text: text });
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,10 +25,16 @@ class App extends Component {
           <div className="card-body">
             <div className="row">
               <div className="col-md-6">
-                <textarea className="form-control" value={ sampleText } rows="35"></textarea>
+                <h2 className="text-center">Write in markdown here</h2>
+                <textarea
+                className="form-control"
+                value={ this.state.text }
+                rows="35"
+                onChange={(e) => this.editText(e)}>
+                </textarea>
               </div>
               <div className="col-md-6">
-                <h2>Live preview</h2>
+                <h2 className="text-center">Live preview</h2>
               </div>
             </div>
           </div>
