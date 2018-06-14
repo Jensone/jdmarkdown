ed6e2e116b18ec93c4de7ff1f902f5278c87196e
Jensone <hello@jensone.com>
2018-06-14 19:03:27 +0200

    Ajout du footer et du lien d'analytic Heap

--- src/Editor.js
+++ src/Editor.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './Editor.css';
 // MD insctructions
 import { mdExample } from './mdExample';
@@ -36,8 +35,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <br/>
-        <div className="card">
+
+        <div className="col-xs-12 text-center text-white d-block d-sm-none">
+          <h3>Best experience on dekstop or tablet</h3>
+        </div>
+
+        <div className="card d-none d-sm-block">
           <div className="card-body">
             <div className="row">
               <div className="col-md-6">
@@ -56,7 +59,25 @@ class App extends Component {
             </div>
           </div>
         </div>
-        <br/>
+
+        <div className="card">
+          <footer className="row">
+            <div className="col-sm-12 col-md-4 text-center">
+              <a href="https://www.infomaniak.com/goto/fr/my-easy-site?utm_term=5ae406817ba98"><img src="https://storage-master.infomaniak.ch/promotional/banners/medium_rectanglewordpress_fr.jpg" className="infomaniak"/></a>
+            </div>
+            <div className="col-sm-12 col-md-4 text-center">
+              <h4>Made by <a href="http://jensone.com" target="_blank">JENSONE</a></h4>
+              <a href="http://jensone.com" target="_blank"><img src="http://jensone.com/wp-content/uploads/2017/11/Logo-J.svg" alt="Logo Jensone" width="180"/></a>
+            </div>
+            <div className="col-sm-12 col-md-4 text-center">
+              <h5>Cooked with</h5>
+              <p className="text-center"><img src="http://logos-download.com/wp-content/uploads/2016/09/React_logo_wordmark.png" alt="logo ReactJS" width="180"/></p>
+              <h5>Analytics by</h5>
+              <p className="text-center"><img src="https://heapanalytics.com/img/styleguide/Heap%20Logo@2x.png" alt="Logo heap analytics" width="180"/></p>
+            </div>
+          </footer>
+        </div>
+
       </div>
     );
   }
